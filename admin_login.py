4f50560326612b03f91046358eb852756eb1f431
romwil <romwil.pilapil@gmail.com>
2020-12-16 20:17:47 +0800

    beta test add commends

--- admin_login.py
+++ admin_login.py
@@ -4,13 +4,16 @@ print('Admin Log-in:')
 print()
 while True:
 
+    # user input admin and passcode
     user_admin = input('admin: ')
     user_admin = user_admin.title()
     passcode = input('passcode: ')
     print()
 
+    # admit message
     if user_admin in list(admin_dict.keys()) and passcode in list(admin_dict.values()):
         print('beta test project admit by', user_admin)
         break
 
+    # error message log-in
     print('Inavalid user or passcode, try again')
